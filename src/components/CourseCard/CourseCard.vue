7e74eac3cb127640531e6671d302fa98503a43d7
ksharifbd <ksharifbd@gmail.com>
2020-09-29 08:41:31 +0600

    adds click hnadler

--- src/components/CourseCard/CourseCard.vue
+++ src/components/CourseCard/CourseCard.vue
@@ -2,7 +2,9 @@
   <Card showFooter>
     <template v-slot:cardHeader> This is card header </template>
     <template v-slot:cardBody> This is card Body </template>
-    <template v-slot:cardFooter>This is card footer </template>
+    <template v-slot:cardFooter>
+      <button name="random" v-on:click="handleClick">Click me</button>
+    </template>
   </Card>
 </template>
 
@@ -12,6 +14,9 @@ import Card from "../Card/Card";
 export default {
   components: {
     Card,
+  },
+  methods: {
+    handleClick: (e) => console.log(e.target),
   },
 };
 </script>
